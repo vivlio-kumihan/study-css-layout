0f0efce984c4dfaebe051f929802c0a407867397
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-13 16:08:55 +0900

    grid-auto-flow: colunm; => gridの流れ方向を縦に変える, dense => 空きを埋める

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,49 +1,44 @@
-// 配列内の値の順をランダムにする関数
-function arrShuffle(array) {
-  // array.length - 1 => 最後の値のインデックス番号
-  for (let i = (array.length - 1); 0 < i; i--) {
-    // 0〜(i+1)未満の範囲でランダムな値を取得
-    let r = Math.floor(Math.random() * (i + 1));
-    let tmp = array[i];
-    array[i] = array[r];
-    array[r] = tmp;
-  }
-  return array;
-}
+// // 配列内の値の順をランダムにする関数
+// function arrShuffle(array) {
+//   // array.length - 1 => 最後の値のインデックス番号
+//   for (let i = (array.length - 1); 0 < i; i--) {
+//     // 0〜(i+1)未満の範囲でランダムな値を取得
+//     let r = Math.floor(Math.random() * (i + 1));
+//     let tmp = array[i];
+//     array[i] = array[r];
+//     array[r] = tmp;
+//   }
+//   return array;
+// }
 
-// ターゲットとなる全てのリストを収集する。
-const container = document.querySelector(".grid-container")
-let lists = container.children
+// // ターゲットとなる全てのリストを収集する。
+// const container = document.querySelector(".grid-container")
+// let lists = container.children
 
-container.addEventListener("mouseenter", function() {
-  // ランダムな順番を格納した配列を生成する。
-  let nums = []
-  for (let i = 1; i < lists.length + 1; i++) {
-    nums.push(i)
-  }
-  let ramdomNums = arrShuffle(nums)
+// container.addEventListener("mouseenter", function() {
+//   // ランダムな順番を格納した配列を生成する。
+//   let nums = []
+//   for (let i = 1; i < lists.length + 1; i++) {
+//     nums.push(i)
+//   }
+//   let ramdomNums = arrShuffle(nums)
   
-  // リストを最初から呼び出して、スタイルにランダムな数字を入れていく。
-  for (let i = 0; i < lists.length; i++) {
-    lists[i].setAttribute("style", `order:${ ramdomNums[i] }`)
-  }
-})
+//   // リストを最初から呼び出して、スタイルにランダムな数字を入れていく。
+//   for (let i = 0; i < lists.length; i++) {
+//     lists[i].setAttribute("style", `order:${ ramdomNums[i] }`)
+//   }
+// })
 
-container.addEventListener("mouseleave", function() {
-  // ランダムな順番を格納した配列を生成する。
-  let nums = []
-  for (let i = 1; i < lists.length + 1; i++) {
-    nums.push(i)
-  }
-  let ramdomNums = arrShuffle(nums)
+// container.addEventListener("mouseleave", function() {
+//   // ランダムな順番を格納した配列を生成する。
+//   let nums = []
+//   for (let i = 1; i < lists.length + 1; i++) {
+//     nums.push(i)
+//   }
+//   let ramdomNums = arrShuffle(nums)
   
-  // リストを最初から呼び出して、スタイルにランダムな数字を入れていく。
-  for (let i = 0; i < lists.length; i++) {
-    lists[i].setAttribute("style", `order:${ ramdomNums[i] }`)
-  }
-})
-
-// const pic = document.getElementById("cover")
-// pic.addEventListener("mouseenter", function () {
-//   this.src = "../img/p2.jpg"
-// }, false)
+//   // リストを最初から呼び出して、スタイルにランダムな数字を入れていく。
+//   for (let i = 0; i < lists.length; i++) {
+//     lists[i].setAttribute("style", `order:${ ramdomNums[i] }`)
+//   }
+// })
