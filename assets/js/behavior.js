5c034f8a87a146c4ff1dc434a9151cd7d433c7d9
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-20 23:07:44 +0900

    HTMLに画像を記述してCSSで枠内位置をコントロールできる設定

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,4 +1,22 @@
-// font-size: 15px;の正数倍の早見表を作るためコード
-for (let i = 1; i <= 70; i++) {
-  // console.log(i * 15)
+let heads = document.querySelectorAll("h2")
+for (headingTwo of heads) {
+  let lastChar =  headingTwo.textContent.slice(-1)
+  let preText =  headingTwo.textContent.slice(0, -1)
+  headingTwo.innerHTML = `${ preText }<span class="remove-letter-spacing">${ lastChar }</span>`
 }
+
+const dts = document.querySelectorAll("dt")
+for (let dt of dts) {
+  dt.addEventListener("mouseenter", function() {
+    dt.classList.add("active")
+  })
+  dt.addEventListener("mouseleave", function() {
+    dt.classList.remove("active")
+  })
+}
+
+// if (dt.classList.contains("active")) {
+//   dt.classList.remove("active")
+// } else {
+//   dt.classList.add("active")
+// }
